65e01a86a96b71b698dafc42f8814376a13192f7
Ahlat85 <ahlatthobai85@gmail.com>
2021-09-24 16:56:44 +0700

    Menambah fitur

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,29 @@
-const youtube_player = function(id) {
-  const _element = document.getElementById(id);
-  const _src = new URL(_element.getAttribute("src"));
+const youtube_player = id => {
+  let _element = document.getElementById(id);
+  let _src = new URL(_element.getAttribute("src"));
 
   const data = {
+    element: {
+      get: () => {
+        return _element;
+      },
+      set: id => {
+        _element = document.getElementById(id);
+      }
+    },
+    src: {
+      //  BUG: Nilai yang dihasilkan adalah parameter start.
+      get: () => {
+        return _element.getCurrentTime;
+      },
+      refresh: () => {
+        data.src.set(_src);
+      },
+      set: url => {
+        _src = new URL(url);
+        _element.setAttribute("src", _src);
+      }
+    },
     time: {
       get: () => {
         return _src.searchParams.get("start");
@@ -10,16 +31,38 @@ const youtube_player = function(id) {
       set: second => {
         const start = _src.searchParams.get("start");
         _src.searchParams.set("start", second);
-        _element.setAttribute("src", _src);
+        data.src.set(_src);
       },
       add: second => {
         const start = _src.searchParams.get("start");
         second = parseInt(start) + parseInt(second);
         _src.searchParams.set("start", second);
-        _element.setAttribute("src", _src);
+        data.time.set(second);
       }
-    }
+    },
   }
 
   return data;
 }
+
+//  Set video controllers button
+(function() {
+  const buttons = Array.from(document.getElementsByClassName("btn-youtube-player"));
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const id = btn.getAttribute("youtube-player-id");
+      const value = btn.getAttribute("youtube-player-value");
+      const action = btn.getAttribute("youtube-player-action")
+      const video = youtube_player(id);
+
+      switch (action) {
+        case "forward":
+          video.time.add(value);
+          break;
+        case "refresh":
+          video.src.refresh();
+          break;
+      }
+    });
+  })
+})()
